a7301f079aa5d3b61b48115442c61670f8f6c3fd
Alejandro <alejandrocartagena68@gmail.com>
2024-02-13 12:58:37 -0500

    Defined a simple movie schema using mongoose

---
+++ app.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    .then(() => console.log("CONNECTION OPEN"))
+    .catch(err => console.log("ERROR: ", err))
+
+const movieSchema = new mongoose.Schema({
+    title:  String,
+    year:   Number,
+    score:  Number,
+    rating: String
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1984, score: 9.2, rating: 'R' })
