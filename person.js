d4c8ef023994e0cbceffab7a6b1ba10c9461d3f9
Alejandro <alejandrocartagena68@gmail.com>
2024-02-13 18:20:05 -0500

    Added virtual method getter and setter for person schema

---
+++ person.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
+    .then(() => console.log("CONNECTION OPEN"))
+    .catch(err => console.log("ERROR: ", err))
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+})
+
+personSchema.virtual('fullname')
+    .get(function () {
+        return `${this.first} ${this.last}`
+    })
+    .set(function (name) {
+        const [first, last] = name.split(' ')
+        this.first = first
+        this.last = last
+    })
+
+const Person = mongoose.model('Person', personSchema)
+
